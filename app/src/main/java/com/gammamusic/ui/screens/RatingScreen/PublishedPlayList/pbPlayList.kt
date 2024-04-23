e9462660d1d0c088fc7abba9db3f997422c8ac35
rikisukamaru <tkonyshev@bk.ru>
2024-04-23 16:06:03 +0800

    Рабочий билд 23.04.24 Было реализовано окно самого опубликованного плейлиста , к каждому элементы lazy column были добавлены свайпы для системы рейтинга. Впринципе подготовка закончилась, пора за работу))

--- app/src/main/java/com/gammamusic/ui/screens/RatingScreen/PublishedPlayList/pbPlayList.kt
+++ app/src/main/java/com/gammamusic/ui/screens/RatingScreen/PublishedPlayList/pbPlayList.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
